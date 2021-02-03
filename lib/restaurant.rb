7c2e9edcff15f5eece41160f699452e88e26775d
koshea9 <keegan.oshea9@gmail.com>
2021-02-03 13:46:41 -0700

    Add iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,14 +1,24 @@
 class Restaurant
   attr_reader :opening_time,
               :name,
-              :dishes
+              :dishes,
+              :closing_time
 
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
+    @closing_time = "00:00"
+
   end
 
+#need to account for edge case where total > 24:00
+  def closing_time(hours)
+    "#{(opening_time.to_i + hours)}:00"
+  end
 
+  def add_dish(dish)
+    @dishes << dish
+  end
 
 end
